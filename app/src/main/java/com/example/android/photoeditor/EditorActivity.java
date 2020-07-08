292836852c3dd63a9dd482b42639bbb053cd1ce0
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-07-08 10:53:33 +0530

    added filters

--- app/src/main/java/com/example/android/photoeditor/EditorActivity.java
+++ app/src/main/java/com/example/android/photoeditor/EditorActivity.java
@@ -1,37 +1,48 @@
 package com.example.android.photoeditor;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class EditorActivity extends AppCompatActivity {
 
+    private ImageView imageView;
+
+    private Bitmap selected_img;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         /** Getting the Uri of the image passed from the previous intent */
         Intent intent = getIntent();
         Uri uri = intent.getData();
 
-        ImageView imageView = findViewById(R.id.image);
+        imageView = findViewById(R.id.image);
         InputStream in;
 
         /** Setting up the image on the layout */
         try {
             in = getContentResolver().openInputStream(uri);
-            final Bitmap selected_img = BitmapFactory.decodeStream(in);
+            selected_img = BitmapFactory.decodeStream(in);
             imageView.setImageBitmap(selected_img);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -39,4 +50,113 @@ public class EditorActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                // Do nothing
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void defaultImage (View view) {
+        imageView.setImageBitmap(selected_img);
+    }
+
+    public void filter1 (View view) {
+        imageView.setImageBitmap(doInvert(selected_img));
+    }
+
+    public void filter2 (View view) {
+        imageView.setImageBitmap(createContrast(selected_img, 50));
+    }
+
+    public Bitmap doInvert(Bitmap src) {
+        // create new bitmap with the same settings as source bitmap
+        Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        // color info
+        int A, R, G, B;
+        int pixelColor;
+        // image size
+        int height = src.getHeight();
+        int width = src.getWidth();
+
+        // scan through every pixel
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                // get one pixel
+                pixelColor = src.getPixel(x, y);
+                // saving alpha channel
+                A = Color.alpha(pixelColor);
+                // inverting byte for each R/G/B channel
+                R = 255 - Color.red(pixelColor);
+                G = 255 - Color.green(pixelColor);
+                B = 255 - Color.blue(pixelColor);
+                // set newly-inverted pixel to output image
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+
+        // return final bitmap
+        return bmOut;
+    }
+
+    public Bitmap createContrast(Bitmap src, double value) {
+        // image size
+        int width = src.getWidth();
+        int height = src.getHeight();
+        // create output bitmap
+        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+        // color information
+        int A, R, G, B;
+        int pixel;
+        // get contrast value
+        double contrast = Math.pow((100 + value) / 100, 2);
+
+        // scan through all pixels
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                // get pixel color
+                pixel = src.getPixel(x, y);
+                A = Color.alpha(pixel);
+                // apply filter contrast for every channel R, G, B
+                R = Color.red(pixel);
+                R = (int)(((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if(R < 0) { R = 0; }
+                else if(R > 255) { R = 255; }
+
+                G = Color.red(pixel);
+                G = (int)(((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if(G < 0) { G = 0; }
+                else if(G > 255) { G = 255; }
+
+                B = Color.red(pixel);
+                B = (int)(((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if(B < 0) { B = 0; }
+                else if(B > 255) { B = 255; }
+
+                // set new pixel color to output bitmap
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+
+        // return final image
+        return bmOut;
+    }
+
 }
